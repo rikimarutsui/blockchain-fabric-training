c446cc381a5eeefdfc9e6c747d48b9f56fb34fd9
TKTsui <tktsui@hko>
2019-08-01 16:52:57 +0800

    implement product page

--- javascript/myapp/routes/products.js
+++ javascript/myapp/routes/products.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const FabricClient = require('../fabric/client.js');
+const client = new FabricClient();
 
 router.get('/create', function(req, res, next) {
   res.render('createProduct');
@@ -7,7 +9,22 @@ router.get('/create', function(req, res, next) {
 
 /* GET products listing. */
 router.get('/:productId', function(req, res, next) {
-    res.render('product');
-});
+  var productRequest = new Promise((resolve, reject) => {
+    try{
+      var product = client.query('admin', 'queryProduct', [req.params.productId]);
+      resolve(product);
+    }catch(error){
+      reject(error);
+    }
+  });
+  var userRequest = new Promise((resolve, request) => {
+    var users = client.listUsers();
+    resolve(users);
+  })
+    Promise.all([productRequest, userRequest]).then((data) => {
+      res.render('product', {product: data[0], users: data[1]} );
+    });
+
+  });
 
 module.exports = router;
